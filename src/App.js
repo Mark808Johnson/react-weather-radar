1af884ac8744b08776af12c957ac23e96227eaff
Mark Johnson <mark.johnson@etteplan.com>
2023-05-11 12:53:02 +0300

    Removed status bar reference

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Header from './components/Header/Header'
-import StatusBar from './components/StatusBar/StatusBar';
 import WeatherRadar from './components/WeatherRadar/WeatherRadar';
 import WeatherProvider from './context/WeatherContext';
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <>
       <WeatherProvider>
-        {/* <StatusBar /> */}
         <Header />
         <WeatherRadar />
       </WeatherProvider>
